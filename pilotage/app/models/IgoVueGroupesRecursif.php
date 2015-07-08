d514162a7ab56c25b13f9c97d23ef1231e055227
brin01 <nicolas.brisebois-tetreault@msp.gouv.qc.ca>
2015-07-08 14:52:23 -0400

    Code pour générer les métadata

--- pilotage/app/models/IgoVueGroupesRecursif.php
+++ pilotage/app/models/IgoVueGroupesRecursif.php
@@ -37,12 +37,30 @@ class IgoVueGroupesRecursif extends \Phalcon\Mvc\Model {
      */
     public function initialize() {
         $this->setSchema($this->getDI()->getConfig()->database->schema);
+        
+        // Recette pour obtenir les médatada ($m)    
+        // Décommenter les lignes suivantes et accéder l'arborescence d'un 
+        // profil dans le pilotage
+        $m = ($this->getModelsMetaData()->readMetaData($this));
+        echo json_encode($m);
+        die();
     }
     
     public function refresh(){
         
         $this->getDI()->get('db')->execute('REFRESH MATERIALIZED VIEW igo_vue_groupes_recursif') ;
 
+    }
+    
+     /**
+     * Spécifier explicitement les métadata parce que Phalcon ne supporte pas 
+     * les vues matérialisées
+     * @return onject
+     */
+    public function metaData(){
+      // Nécessaire parce que phalcon a de la misère avec les vues matérialisées
+      $m = '{"0":["groupe_id","nom","parent_groupe_id","nom_complet","est_exclu_arbre","grp"],"1":[],"2":["groupe_id","nom","parent_groupe_id","nom_complet","est_exclu_arbre","grp"],"3":[],"4":{"groupe_id":0,"nom":2,"parent_groupe_id":0,"nom_complet":2,"est_exclu_arbre":8,"grp":6},"5":{"groupe_id":true,"parent_groupe_id":true},"8":false,"9":{"groupe_id":1,"nom":2,"parent_groupe_id":1,"nom_complet":2,"est_exclu_arbre":5,"grp":2},"10":[],"11":[]}';
+      return json_decode($m, true);    
     }
 
 }
