140a5873b6775421a75299e6418805fa23765898
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2015-08-04 10:08:10 -0400

    Update loader.php

--- edition/app/config/loader.php
+++ edition/app/config/loader.php
@@ -12,7 +12,7 @@ $loader->registerDirs(
         $config->application->edition->utilDir,        
         $config->application->edition->servicesDir,        
         $config->application->edition->exemplesServicesDir,
-        $config->application->edition->controllersDir,
+        $config->application->edition->customServicesDir,
         $config->application->services->controllersDir
     )
 )->register();
